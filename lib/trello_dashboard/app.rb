331e5d23e2212ca846dde85c5b337b4ca88978e7
badmanski <stanislav.gorski@gmail.com>
2016-03-07 21:53:14 +0200

    Render trello cards on dashboard

--- lib/trello_dashboard/app.rb
+++ lib/trello_dashboard/app.rb
@@ -1,6 +1,11 @@
 module TrelloDashboard
   class App < Sinatra::Base
     get '/' do
+      @lists = {}
+      Sinatra::Application.secrets['lists'].map do |category, ids|
+        @lists[category] = ids.map { |id| Trello::List.find(id) }
+      end
+
       haml :index
     end
   end
